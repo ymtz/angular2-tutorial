a65c88ad900fdb47c0f23b7ff07a39d76e6119dc
Matsumura Yoshiaki <y.matsumura@pigikuro.com>
2016-12-01 13:04:19 +0900

    first commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Tutorial2Page } from './app.po';
+
+describe('tutorial2 App', function() {
+  let page: Tutorial2Page;
+
+  beforeEach(() => {
+    page = new Tutorial2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
